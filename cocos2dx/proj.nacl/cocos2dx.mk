7cafb4a2da85ee91925cdd208c030e0eb2f57826
Sam Clegg <sbc@google.com>
2013-02-20 12:51:26 -0800

    [NaCl] Compile against the new naclports bundle
    This is achrived by moving LDFLAGS setup into the
    common .mk file and adding paths from $NACL_SDK_ROOT/ports
    to include and link paths.
    
    TBR=binji
    BUG=None
    
    Review URL: https://codereview.chromium.org/12313004

--- cocos2dx/proj.nacl/cocos2dx.mk
+++ cocos2dx/proj.nacl/cocos2dx.mk
@@ -1,3 +1,16 @@
+# This makefile is included by each of the cocos2d-x libraries and examples
+# when building for Native Client.  It defines a set of variables that all
+# cocos2dx projects have in common.
+
+NACL_LIBC = newlib
+NACL_ARCH ?= x86_64
+NACL_AR ?= $(NACL_ARCH)-nacl-ar
+NACL_CC ?= $(NACL_ARCH)-nacl-gcc
+NACL_CXX ?= $(NACL_ARCH)-nacl-g++
+CCFLAGS ?= -Wall
+CXXFLAGS ?= -Wall
+ARFLAGS ?= cr
+
 THIS_MAKEFILE := $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
 
 # The top level of the cocos2dx-x source tree.  The parent Makefile will
@@ -5,8 +18,12 @@ THIS_MAKEFILE := $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
 # THIS_MAKEFILE
 COCOS_ROOT ?= $(realpath $(dir $(THIS_MAKEFILE))/../..)
 
+NACLPORTS_ROOT ?= $(NACL_SDK_ROOT)/ports
+NACLPORTS_INCLUDE ?= $(NACLPORTS_ROOT)/include
 OBJ_DIR ?= out/$(NACL_ARCH)
 LIB_DIR ?= $(COCOS_ROOT)/lib/nacl
+
+INCLUDES += -I$(NACL_SDK_ROOT)/include -I$(NACLPORTS_INCLUDE) -I$(NACLPORTS_INCLUDE)/libxml2
 
 ifeq ($(DEBUG), 1)
 BIN_DIR = bin/debug
@@ -14,24 +31,27 @@ CCFLAGS += -g3 -O0
 CXXFLAGS += -g3 -O0
 LIB_DIR := $(LIB_DIR)/Debug
 OBJ_DIR := $(OBJ_DIR)/Debug
+MULTILIB_SUFFIX := $(NACL_LIBC)_$(NACL_ARCH)/Debug
 DEFINES += -DDEBUG -DCOCOS2D_DEBUG=1
-STATICLIBS = -L$(NACL_SDK_ROOT)/lib/$(NACL_LIBC)_$(NACL_ARCH)/Debug
 else
 BIN_DIR = bin/release
 CCFLAGS += -O3
 CXXFLAGS += -O3
 LIB_DIR := $(LIB_DIR)/Release
 OBJ_DIR := $(OBJ_DIR)/Release
+MULTILIB_SUFFIX := $(NACL_LIBC)_$(NACL_ARCH)/Release
 DEFINES += -DNDEBUG
-STATICLIBS = -L$(NACL_SDK_ROOT)/lib/$(NACL_LIBC)_$(NACL_ARCH)/Release
 endif
+
+# The default library search path consists of the cocos2dx library path, the
+# main nacl sdk library path and the naclports library path.
+LDFLAGS += -L$(LIB_DIR)
+LDFLAGS += -L$(NACL_SDK_ROOT)/lib/$(MULTILIB_SUFFIX)
+LDFLAGS += -L$(NACLPORTS_ROOT)/lib/$(MULTILIB_SUFFIX)
 
 # Some cococs sources use #pragma mark
 CCFLAGS += -Wno-unknown-pragmas
 CXXFLAGS += -Wno-unknown-pragmas
-
-SOUNDLIBS = -lalut -lopenal -lvorbisfile -lvorbis -logg
-FONTLIBS = -lfreetype
 
 ifdef NACL_MOUNTS
 DEFINES += -DOLD_NACL_MOUNTS
@@ -40,9 +60,9 @@ else
 STATICLIBS += -lnacl_io
 endif
 
-STATICLIBS += $(SOUNDLIBS) $(FONTLIBS) -lxml2 -lpng -ljpeg -ltiff -llua 
+SOUNDLIBS := -lalut -lopenal -lvorbisfile -lvorbis -logg
+STATICLIBS += $(SOUNDLIBS) -lfreetype -lxml2 -lpng -ljpeg -ltiff -llua
 STATICLIBS += -lppapi_gles2 -lppapi -lppapi_cpp -lnosys
-SHAREDLIBS += -L$(LIB_DIR)
 SHAREDLIBS += -lpthread -lcocosdenshion -lcocos2d -lz
 
 OBJECTS := $(SOURCES:.cpp=.o)
